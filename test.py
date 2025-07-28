99780a60fc4a14c690874a0acdd8dfefb04e8fba
edgahhhh <ehernandez7419@gmail.com>
2025-07-27 18:20:09 -0600

    added heartbeat

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
 
 from px4_msgs.msg import OffboardControlMode
 
 class OffboardControl(node):
+
+
+
     
