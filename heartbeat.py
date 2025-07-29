065e5bcb0d7645f3d2ac6b12dcb35dcd95be7667
edgahhhh <ehernandez7419@gmail.com>
2025-07-29 09:04:30 -0600

    heartbeat and test

--- heartbeat.py
+++ heartbeat.py
@@ -1,1 +1,16 @@
-
+import rclpy
+from rclpy.node import Node
+from px4_msgs.msg import OffboardControlMode
+ 
+class HeartbeatPublisher(Node):
+    """ Creating a ros2 node called heartbeat_publisher_node"""
+    def __init__(self):
+        super().__init__('heartbeat_publisher_node')
+        
+        self.publisher = self.create_publisher(
+            OffboardControlMode, '/fmu/in/offboard_control_mode', 10)
+        
+        self.timer = self.create_timer(
+            1.0, self.publish_heartbeat)
+        
+        self.get_logger().info('heartbeat_publisher_node started')
--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
-
 from px4_msgs.msg import OffboardControlMode
 
-class OffboardControl(node):
 
+class OffboardControl(Node):
+    """ Creating a ros2 node called offboard_control_node"""
+    def __init__(self):
+        super().__init__('offboard_control_node')
+        
+        self.offboard_pub = self.create_publisher(
+            OffboardControlMode, '/fmu/in/offboard_control_mode', 10)
 
 
     
