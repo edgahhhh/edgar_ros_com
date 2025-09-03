d4670d92bce828b32d5a633c2f1aca2e84006935
robotsmachine <ehernandez7419@gmail.com>
2025-09-03 16:39:11 -0600

    updated and tested trajectory script

--- offboard_py/offboardcontrol_trajectory_setpoints_sinetraj.py
+++ offboard_py/offboardcontrol_trajectory_setpoints_sinetraj.py
@@ -1,10 +1,13 @@
 # OFFBOARDCONTROL_POSITION_SETPOINTS.py
 # This script is to enable offboard control upon execution and send some position set point to the simulator
 
-import rclpy        # RCLPY wiki is your bible here
+import rclpy
 from rclpy.node import Node
+
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
+
 from px4_msgs.msg import OffboardControlMode, TrajectorySetpoint, VehicleLocalPosition, VehicleStatus, VehicleCommand
+
 import numpy as np
 
 class OffboardControl(Node):
@@ -33,28 +36,41 @@ class OffboardControl(Node):
         self.vehicle_local_position_subscriber = self.create_subscription(
             VehicleLocalPosition, '/fmu/out/vehicle_local_position', self.vehicle_local_position_callback, qos_profile
         )
-
-        self.vehicle_stats_subscriber = self.create_subscription(
+        self.vehicle_status_subscriber = self.create_subscription(
             VehicleStatus, '/fmu/out/vehicle_status', self.vehicle_status_callback, qos_profile
         )
-
+        self.timer_period = 0.1
         # Creating a timer and logger
         self.timer = self.create_timer(
-            0.1 , self.timer_callback)
-
+            self.timer_period , self.timer_callback)
         self.get_logger().info('offboard_control_node started') 
 
         # Initialize variables
         self.vehicle_local_position = VehicleLocalPosition()
         self.vehicle_status = VehicleStatus()
+
         self.offboard_setpoint_counter = 0
-        self.timer_reset_sec = 60   # Time to generate a new heading
-        self.counter_reset_discrete = self.timer_reset_sec / (self.timer.timer_period_ns/1000000000) + 1 
-        self.counter = 0
-        self.chase_dist_m = 25 # distance for heading set point in m
-        self.x_position = 0
+
+        # X and Z trajectories share the same period and time constants
+        self.traj_t = 0
+        self.period = 50  # s
+
+        # z_trajectory as function of time
+        # Sine wave w/ amplitude of altitude change over some period
+        self.z_min = -50
+        self.z_amp = -10  # m
+        self.omega = 2 * np.pi / self.period 
+
+        # x_trajectory as function of time
+        # Linear change of target x
+        self.x_min = 0
+        self.x_target = -1000 # m
+        self.dx = self.x_target / self.period   # m/s
+
+        # y trajectory
         self.y_position = 0
-        self.z_position = 0
+        # Counter for x position
+        self.x_position_counter = 0
 
     # Heartbeat signal publisher
     def publish_heartbeat(self):
@@ -70,7 +86,6 @@ class OffboardControl(Node):
     # Position callback
     def vehicle_local_position_callback(self, vehicle_local_position):
         self.vehicle_local_position = vehicle_local_position
-
     # Vehicle status callback
     def vehicle_status_callback(self, vehicle_status):
         self.vehicle_status = vehicle_status
@@ -102,54 +117,89 @@ class OffboardControl(Node):
             VehicleCommand.VEHICLE_CMD_DO_SET_MODE, param1=1.0, param2=6.0)
         self.get_logger().info("Switching to offboard mode")
 
-    def publish_trajectory_setpoint(self, x: float, y: float, z: float, ):
+    def publish_trajectory_setpoint(self, x:float, y:float, z:float, vx:float, vy:float, vz:float):
         """Publish the trajectory setpoint."""
         msg = TrajectorySetpoint()
-        msg.position = [x, y, z]
-        msg.yaw = float(0)  # 90 degree yaw
+        msg.position[0] = x
+        msg.position[1] = y
+        msg.position[2] = z
+
+        msg.velocity[0] = vx
+        msg.velocity[1] = vy
+        msg.velocity[2] = vz
+
+        # msg.acceleration[0] = float(0)
+        # msg.acceleration[1] = float(0)
+        # msg.acceleration[2] = float(0)
+
+        # msg.jerk[0] = float(0)
+        # msg.jerk[1] = float(0)
+        # msg.jerk[2] = float(0)
+
+        msg.yaw = float(0)
+        msg.yawspeed = float(0)
         msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
         self.trajectory_setpoint_publisher.publish(msg)
-        self.get_logger().info(f"Publishing position setpoints {[x, y, z]}")
-
-    # White trash a discrete counter because I'm still learning python
-    def resettable_counter(self):
-        # Count up if below reset threshold
-        if  self.counter < self.counter_reset_discrete and self.vehicle_status.nav_state == VehicleStatus.NAVIGATION_STATE_OFFBOARD:
-            self.counter += 1
-        # Reset if at or above the reset threshold
-        elif self.counter >= self.counter_reset_discrete and self.vehicle_status.nav_state == VehicleStatus.NAVIGATION_STATE_OFFBOARD:
-            self.counter = 1
-
+        self.get_logger().info(f"Publishing position setpoints: {[x, y, z]} \nPublishing velocity setpoints {[vx, vy, vz]}")
 
     def timer_callback(self):
         """ Timer callback to publish heartbeat and trajectory commands """
         self.publish_heartbeat()
-        self.resettable_counter()
 
-        if self.offboard_setpoint_counter == 10:
+        if self.offboard_setpoint_counter == 20:
             self.engage_offboard_mode()
 
-        if self.offboard_setpoint_counter < 11:
+        if self.offboard_setpoint_counter < 21:
             """ Attempt to engage offbaord mode """
             self.offboard_setpoint_counter += 1
 
         if self.vehicle_status.nav_state == VehicleStatus.NAVIGATION_STATE_OFFBOARD:
-            """ Command forward moving x, some altitude, and some forward velocity to achieve """
+            """ Command altitude change as a wave and a forward moving x point, holding y. Vehicle
+            should move in xz plane only """
 
-            self.x_position = self.vehicle_local_position.x + self.chase_dist_m
+            if self.x_position_counter == 0:
+                self.x_min = self.vehicle_local_position.x
+            if self.x_position_counter < 1:
+                self.x_position_counter += 1
 
-            self.publish_position_setpoint( 
-                float(0),                     # x position, chase
-                float(0),                     # y position, hold
-                float(self.z_position))                     # z position, hold
+            self.traj_t = self.traj_t + self.timer_period
+            self.sine_calc = np.sin(self.omega * self.traj_t)
+
+            self.x_position = self.x_min + self.dx*self.traj_t
+            self.z_position = self.z_min + self.z_amp*self.sine_calc
+
+            self.x_speed = self.dx
+            self.y_speed = 0
+            self.z_speed = self.omega*self.z_amp*np.cos(self.omega*self.traj_t)
+
+
+            self.x_position = 0
+            self.z_position = -80
+
+            self.x_speed = 0
+            self.z_speed = 0
+
+            self.publish_trajectory_setpoint(
+                x = float(self.x_position),
+                y = float(self.y_position),
+                z = float(self.z_position),
+                vx = float(self.x_speed),
+                vy = float(self.y_speed),
+                vz = float(self.z_speed)
+                )
 
 
 def main(args=None):
     print('Starting heartbeat signal node... ')
+
     rclpy.init(args = args)
+
     offboard_control = OffboardControl()
+
     rclpy.spin(offboard_control)
+
     offboard_control.destroy_node()
+
     rclpy.shutdown()
 
 if __name__ == '__main__':
